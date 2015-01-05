7e86ef1f9d0fb25de392f2eeacdd872e62097256
Sebastian Steinmann <sebastian.steinmann@bekk.no>
2015-01-05 15:09:04 +0100

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var changed = require('gulp-changed');
+
+var basePath = "../dkfpilot/src/main/projects/Eika.DKF.Web.Portal";
+var prodPath = "../../websites/terra.eikadev.no/Website";
+
+var paths = {
+    sass: '/Content/Styles/scss',
+    css: '/Content/Styles/css',
+    js:  "/Scripts",
+    templates: "/Presentations"
+};
+
+gulp.task("templates", function() {
+    gulp.src(basePath + paths.templates + "/**/*.ascx")
+        .pipe(changed(prodPath + paths.templates))
+        .pipe(gulp.dest(prodPath + paths.templates))
+})
+
+gulp.task('js', function() {
+    gulp.src(basePath + paths.js + '/**/*.js')
+        .pipe(changed(prodPath + paths.js))
+        .pipe(gulp.dest(prodPath + paths.js))
+});
+
+gulp.task('sass', function () {
+    gulp.src(basePath + paths.sass + '/*.scss')
+        .pipe(sass({
+            style: 'compact',
+            sourcemapPath: "",
+            sourcemap: false
+        }))
+        .pipe(gulp.dest(basePath + paths.css))
+});
+
+gulp.task('sass-dev', function () {
+    gulp.src(basePath + paths.sass + '/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            sourcemapPath: "",
+            sourcemap: false
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(prodPath + paths.css));
+});
+
+gulp.task('startWatch', function () {
+    gulp.watch(basePath + paths.sass + '/**/*.scss', ['sass', 'sass-dev']);
+    gulp.watch(basePath + paths.js + '/**/*.js', ['js']);
+    gulp.watch(basePath + paths.templates + '/**/*.ascx', ['templates']);
+});
+
+gulp.task('watch', ['js', 'sass', 'sass-dev', 'templates', 'startWatch']);
+
+gulp.task('default', ['sass', 'js']);
